d50919d02f8b43e7e29777696549cfe4bd7e82d0
Jacob-Idolor <jacob.idolor@outlook.com>
2019-04-22 15:33:45 -0700

    Add files via upload

---
+++ magicballround2.py
@@ -0,0 +1,11 @@
+import random
+messages = ['It is Certain',
+            'It is Decided',
+            'Ask Again Later',
+            'Very Doubtful',
+            'My Reply is No',
+            'yo Mama',
+            'Outlook not looking good',
+            ]
+print(messages[random.randint(0,len(messages) - 1)])
+
